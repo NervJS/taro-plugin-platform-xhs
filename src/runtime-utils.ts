8ae1d59741263c9ec6381aa4ce2075728c3d6504
hadi <tailiang@xiaohongshu.com>
2022-08-04 16:37:45 +0800

    fix: add page default method onShareChat

--- src/runtime-utils.ts
+++ src/runtime-utils.ts
@@ -10,4 +10,19 @@ export const hostConfig = {
   getPathIndex (indexOfNode) {
     return `${indexOfNode}`
   },
+  getMiniLifecycle (config) {
+    const methods = config.page[5]
+    /**
+     * 兼容 taro 场景下 onShareChat 不支持的问题
+     * 
+     * @warning 这个变更的缺陷会导致 taro 场景默认都支持 onShareChat (微信好友分享) 的功能
+     * 
+     * 如果需要按照 taro 官方 onShareAppMessage, onShareTimeline 的方式走配置化方式
+     * 需要在 taro 底层进行统一，详见 @link {https://github.com/NervJS/taro/issues/12217}
+     */
+    if (methods.indexOf('onShareChat') === -1) {
+      methods.push('onShareChat')
+    }
+    return config
+  }
 }
